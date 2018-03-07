844c82d55df3db745caee9decae63b39c6fa08b6
Dominik Hug <h.dom90@gmail.com>
2018-03-07 11:30:13 +0100

    initial Player class with positioning of the player

--- PrimitiveType.java
+++ PrimitiveType.java
@@ -14,7 +14,8 @@ class PrimitiveType {
     public static void main(String[] args) {
         Console.clearScreen();
         Word word = new Word();
-        word.initialize(0, 50, "sajt");
+        //word.initialize(0, 50, "☗");
+        word.initialize(0,50, "◢☗◣");
         Timer timer = new Timer();
         TimerTask task = new TimerTask() {
             public void run() {
@@ -23,17 +24,4 @@ class PrimitiveType {
         };
         timer.schedule(task, 0L, 1000L / wordSpeed);
     }
-
-    private Character tryToRead() {
-        try {
-            if (System.in.available() > 0) {
-                return (char)System.in.read();
-            }
-        }
-        catch (IOException e) {
-            System.err.println("Error " + e.getMessage());
-        }
-        return null;
-    }
-    
 }
--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@ echo -e "\033[?25l"
 stty raw -echo
 javac -cp QuadEngine.jar:. *.java
 java -cp QuadEngine.jar:. PrimitiveType
+java -cp QuadEngine.jar:. Player
 stty cooked echo
 echo -e "\033[?25h"
